2790c906f21c9f6fa24bd603ff8058181b0e35f3
RATIARIVELO Nekena Rayane <rratiarivelo@gmail.com>
2026-04-12 16:18:19 +0300

    use timezone on datetime

--- template/src/infrastructure/persistence/database/models/base_models.py
+++ template/src/infrastructure/persistence/database/models/base_models.py
@@ -1,20 +1,27 @@
 from datetime import datetime
 from typing import Optional
-from sqlmodel import Field, SQLModel, func
+from sqlmodel import Field, SQLModel
+from sqlalchemy import DateTime, func
 
 class TimestampedModel(SQLModel):
     created_at: Optional[datetime] = Field(
         default=None,
+        sa_type=DateTime(timezone=True),  # type: ignore
         sa_column_kwargs={
             "server_default": func.now(),
             "nullable": True,
         },
     )
-    updated_at: datetime = Field(
-        nullable=False,
-        default=func.now(),
-        sa_column_kwargs={"onupdate": func.now(), "server_default": func.now()},
+    updated_at: Optional[datetime] = Field(
+        default=None,
+        sa_type=DateTime(timezone=True),  # type: ignore
+        sa_column_kwargs={
+            "server_default": func.now(),
+            "onupdate": func.now(),
+            "nullable": False,
+        },
     )
+
 
 class BaseSQLModel(TimestampedModel):
     id: int | None = Field(primary_key=True, index=True, default=None)
